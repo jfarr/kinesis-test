27c2b7719461335a5e32bf170ef90934e8d13610
jfarr <jfarr1@godaddy.com>
2018-09-16 15:50:08 -0700

    cleanup

--- src/main/java/com/example/demo/KinesisTestApplication.java
+++ src/main/java/com/example/demo/KinesisTestApplication.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
-import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,88 +31,72 @@ import reactor.core.publisher.Mono;
 @SpringBootApplication
 public class KinesisTestApplication {
 
-	@RestController
-	@RequestMapping("/test")
-	public class TestController {
+  @RestController
+  @RequestMapping("/test")
+  public class TestController {
 
-		@Autowired
-		TestGateway gateway;
-		
-		@PostMapping
-		public Mono<String> test(@RequestBody String request) throws Exception {
-			Map<String, Object> object = new HashMap<String, Object>();
-			object.put("message", request);
-			gateway.send(object);
-			return Mono.just(object.toString());
-		}
-	}
-	
-	@MessagingGateway(defaultRequestChannel = "inputChannel")
-	public interface TestGateway {
-		void send(Object message);
-	}
-	
-	@Bean
-	public IntegrationFlow testFlow() {
-		return IntegrationFlows.from("inputChannel")
-				.log(m -> "received message: " + StringEscapeUtils.escapeJava(m.getPayload().toString()))
-				.channel("kinesisSendChannel")
-				.get();
-	}
-	
-	@Bean
-	public AmazonKinesisAsync kinesis() {
-		return AmazonKinesisAsyncClientBuilder.defaultClient();
-	}
-	
-	@Bean
-	@ServiceActivator(inputChannel = "kinesisSendChannel")
-	public MessageHandler kinesisMessageHandler(AmazonKinesisAsync amazonKinesis) {
-	    KinesisMessageHandler kinesisMessageHandler = new KinesisMessageHandler(amazonKinesis);
-	    kinesisMessageHandler.setStream("test-stream");
-	    kinesisMessageHandler.setPartitionKey("1");
-	    kinesisMessageHandler.setConverter(new TestConverter());
-	    return kinesisMessageHandler;
-	}
-	
-	private final class TestConverter implements Converter<Object, byte[]> {
-		
-		@Override
-		public byte[] convert(Object source) {
-//				return convertProto(convertJson(source));
-			return convertString(convertJson(source));
-		}
+    @Autowired
+    TestGateway gateway;
 
-		private String convertJson(Object source) {
-			try {
-				return new ObjectMapper().writeValueAsString(source);
-			} catch (JsonProcessingException e) {
-				e.printStackTrace();
-				return "";
-			}
-		}
+    @PostMapping
+    public Mono<String> test(@RequestBody String request) throws Exception {
+      Map<String, Object> object = new HashMap<String, Object>();
+      object.put("id", UUID.randomUUID());
+      object.put("message", request);
+      gateway.send(object);
+      return Mono.just(object.toString());
+    }
+  }
 
-		private byte[] convertString(Object source) {
-			return (source.toString() + "\n").getBytes();
-		}
+  @MessagingGateway(defaultRequestChannel = "inputChannel")
+  public interface TestGateway {
+    void send(Object message);
+  }
 
-		private byte[] convertProto(Object source) {
-			try {
-				ByteArrayOutputStream output = new ByteArrayOutputStream();
-				TestProtos.Message.newBuilder()
-					.setText(source.toString())
-					.setMeta("foo")
-					.build()
-					.writeDelimitedTo(output);
-				return output.toByteArray();
-			} catch (Exception e) {
-				e.printStackTrace();
-				return new byte[0];
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(KinesisTestApplication.class, args);
-	}
+  @Bean
+  public IntegrationFlow testFlow() {
+    return IntegrationFlows.from("inputChannel")
+        .log(m -> "received message: " + StringEscapeUtils.escapeJava(m.getPayload().toString()))
+        .channel("kinesisSendChannel").get();
+  }
+
+  @Bean
+  public AmazonKinesisAsync kinesis() {
+    return AmazonKinesisAsyncClientBuilder.defaultClient();
+  }
+
+  @Bean
+  @ServiceActivator(inputChannel = "kinesisSendChannel")
+  public MessageHandler kinesisMessageHandler(AmazonKinesisAsync amazonKinesis) {
+    KinesisMessageHandler kinesisMessageHandler = new KinesisMessageHandler(amazonKinesis);
+    kinesisMessageHandler.setStream("test-stream");
+    kinesisMessageHandler.setPartitionKey("1");
+    kinesisMessageHandler.setConverter(new TestConverter());
+    return kinesisMessageHandler;
+  }
+
+  private final class TestConverter implements Converter<Object, byte[]> {
+
+    @Override
+    public byte[] convert(Object source) {
+      return convertString(convertJson(source));
+    }
+
+    private String convertJson(Object source) {
+      try {
+        return new ObjectMapper().writeValueAsString(source);
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+        return "";
+      }
+    }
+
+    private byte[] convertString(Object source) {
+      return (source.toString() + "\n").getBytes();
+    }
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(KinesisTestApplication.class, args);
+  }
 }
